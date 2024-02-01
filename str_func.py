9be67a97b102911a815ef245ca14385b4cd352a9
Tony <king8sky@gmail.com>
2024-02-02 01:28:04 +1100

    adding a return function

--- str_func.py
+++ str_func.py
@@ -1,4 +1,9 @@
 
 def return_string(value):
-    """возвращает строку верхним регистром"""
+    """возвращает строку регистром"""
     return value.upper()
+
+def capital_letters(value):
+    """делает заглавными первые буквы каждого слова в строке, поступившей на вход"""
+    return ' '.join(word.capitalize() for word in value.split())
+
